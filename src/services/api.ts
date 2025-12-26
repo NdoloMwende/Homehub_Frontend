2852a73772a1d3cce40777d2bd93305d87f28eed
Ndolo Mercy <mercymwendend444@gmail.com>
2025-12-26 21:13:07 +0300

    implement API service and login functionality

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000", // json-server default
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API Error:", error.response || error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
